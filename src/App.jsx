c01428cb138ae6b1097e34c8923d9c190645a4e9
Dmitry Kazantsev <dmitry_kazantsev@yahoo.com>
2024-12-20 17:29:07 +0300

    109 Components, Composition and Reusability - Fixing Prop Drilling With Comosition

--- src/App.jsx
+++ src/App.jsx
@@ -52,20 +52,27 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MoviesList movies={movies} />
+        </ListBox>
+        <WatchBox />
+      </Main>
     </>
   );
 }
 
 // STRUCTURAL COMPONENT
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <>
       <nav className="nav-bar">
         <Logo />
         <Search />
-        <NumResults movies={movies} />
+        {children}
       </nav>
       ;
     </>
@@ -108,18 +115,15 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <>
-      <main className="main">
-        <ListBox movies={movies} />
-        <WatchBox />
-      </main>
+      <main className="main">{children}</main>
     </>
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -127,7 +131,7 @@ function ListBox({ movies }) {
       <button className="btn-toggle" onClick={() => setIsOpen1(open => !open)}>
         {isOpen1 ? '–' : '+'}
       </button>
-      {isOpen1 && <MoviesList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
